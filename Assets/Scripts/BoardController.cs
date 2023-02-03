a3980a09c85b92a947f5d9cb83a44dfeb3919129
Ewnascimento <ewerton_snascimento@outlook.com>
2023-02-03 13:25:06 -0300

    Instantiate PawnWhite

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateTable : MonoBehaviour
+public class BoardController : MonoBehaviour
 {
     [SerializeField]
     private GameObject blackHouse;
@@ -12,6 +12,31 @@ public class CreateTable : MonoBehaviour
 
     [SerializeField]
     private Transform target;
+
+    [SerializeField]
+    private GameObject PawnWhite;
+
+    [SerializeField]
+    private GameObject RookWhite;
+
+    [SerializeField]
+    private GameObject KnightWhite;
+
+    [SerializeField]
+    private GameObject BishopWhite;
+
+    [SerializeField]
+    private GameObject QueenWhite;
+
+    [SerializeField]
+    private GameObject KingWhite;
+
+
+
+
+
+
+
 
     private GameObject[,] board;
 
@@ -23,6 +48,7 @@ public class CreateTable : MonoBehaviour
         board = new GameObject[8, 8];
 
         GenerateGrid();
+        CreatePiece();
     }
 
     void GenerateGrid()
@@ -38,15 +64,28 @@ public class CreateTable : MonoBehaviour
                 if ((x + z) % 2 == 0)
                 {
                     board[x, z] = Instantiate(blackHouse, target.position, target.rotation);
+                    board[x, z].GetComponent<HousesBehaviour>().isWhite = false;
                 }
                 else
                 {
                     board[x, z] = Instantiate(whiteHouse, target.position, target.rotation);
+                    board[x, z].GetComponent<HousesBehaviour>().isWhite = true;
                 }
 
-                board[x,z].name = $"Tile {x} {z}";
-
+                board[x, z].name = $"Tile {x} {z}";
             }
         }
     }
+
+    void CreatePiece()
+    {
+        for (int x = 0; x < 8; x++)
+        {
+            target.position = new Vector3(initPosX + x, 0.2f, initPosZ + 1);
+
+            board[1, x] = Instantiate(PawnWhite,target.position, target.rotation);
+        }
+
+    }
 }
+
---
+++ Assets/Scripts/HousesBehaviour.cs
@@ -0,0 +1,19 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HousesBehaviour : MonoBehaviour
+{
+    public bool isWhite;
+    public string typePiece = "Empty";
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+
+    }
+
+}

5528cf4e75a1f9cb72630772a73484fe5b2e17c5
Ewnascimento <ewerton_snascimento@outlook.com>
2023-04-01 11:21:29 -0300

    Check Knights

--- Assets/Scripts/Pieces Behaviour/Check.cs
+++ Assets/Scripts/Pieces Behaviour/Check.cs
@@ -16,6 +16,14 @@ public class Check : MonoBehaviour
     private bool xReduce;
     private bool zReduce;
 
+    private bool xPlus1ZPlus2;
+    private bool xPlus2ZPlus1;
+    private bool xPlus2ZReduce1;
+    private bool xPlus1ZReduce2;
+    private bool xReduce1ZPlus2;
+    private bool xReduce2ZPlus1;
+    private bool xReduce2ZReduce1;
+    private bool xReduce1ZReduce2;
 
 
 
@@ -114,7 +122,7 @@ public class Check : MonoBehaviour
 
         if (contX < 8 && contZ >= 0)
         {
-            if (!ReferenceController.Instance.CheckEmpty(contX, contZ) && !ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            if (!ReferenceController.Instance.CheckEmpty(contX, contZ) && ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite
                     && (ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Pawn"))
             {
                 ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
@@ -154,7 +162,7 @@ public class Check : MonoBehaviour
 
         if (contX >= 0 && contZ >= 0)
         {
-            if (!ReferenceController.Instance.CheckEmpty(contX, contZ) && !ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            if (!ReferenceController.Instance.CheckEmpty(contX, contZ) && ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite
                     && (ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Pawn"))
             {
                 ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
@@ -178,7 +186,6 @@ public class Check : MonoBehaviour
                 ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
                 xReduceZReduce = true;
             }
-
             else
             {
                 xReduceZReduce = false;
@@ -206,7 +213,6 @@ public class Check : MonoBehaviour
                 ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
                 zPlus = true;
             }
-
             else
             {
                 zPlus = false;
@@ -233,7 +239,6 @@ public class Check : MonoBehaviour
                 ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
                 zReduce = true;
             }
-
             else
             {
                 zReduce = false;
@@ -260,7 +265,6 @@ public class Check : MonoBehaviour
                 ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
                 xPlus = true;
             }
-
             else
             {
                 xPlus = false;
@@ -287,13 +291,164 @@ public class Check : MonoBehaviour
                 ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
                 xReduce = true;
             }
-
             else
             {
                 xReduce = false;
             }
             break;
         }
-    }
 
+        // X + 1, Z + 2
+        contX = gameObject.GetComponent<PieceController>().myX + 1;
+        contZ = gameObject.GetComponent<PieceController>().myZ + 2;
+
+        if (gameObject.GetComponent<PieceController>().myX <= 6 && gameObject.GetComponent<PieceController>().myZ <= 5
+            && !ReferenceController.Instance.CheckEmpty(contX, contZ)
+            && ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Knight"
+            && (!ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            || ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite))
+        {
+            ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
+            xPlus1ZPlus2 = true;
+        }
+        else
+        {
+            xPlus1ZPlus2 = false;
+        }
+
+        // X + 2, Z + 1
+        contX = gameObject.GetComponent<PieceController>().myX + 2;
+        contZ = gameObject.GetComponent<PieceController>().myZ + 1;
+
+        if (gameObject.GetComponent<PieceController>().myX <= 5 && gameObject.GetComponent<PieceController>().myZ <= 6
+            && !ReferenceController.Instance.CheckEmpty(contX, contZ)
+            && ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Knight"
+            && (!ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            || ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite))
+        {
+            ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
+            xPlus2ZPlus1 = true;
+        }
+        else
+        {
+            xPlus2ZPlus1 = false;
+        }
+
+        // X + 2, Z - 1
+        contX = gameObject.GetComponent<PieceController>().myX + 2;
+        contZ = gameObject.GetComponent<PieceController>().myZ - 1;
+
+        if (gameObject.GetComponent<PieceController>().myX <= 5 && gameObject.GetComponent<PieceController>().myZ >= 1
+            && !ReferenceController.Instance.CheckEmpty(contX, contZ)
+            && ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Knight"
+            && (!ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            || ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite))
+        {
+            ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
+            xPlus2ZReduce1 = true;
+        }
+        else
+        {
+            xPlus2ZReduce1 = false;
+        }
+
+        // X + 1, Z - 2
+        contX = gameObject.GetComponent<PieceController>().myX + 1;
+        contZ = gameObject.GetComponent<PieceController>().myZ - 2;
+
+        if (gameObject.GetComponent<PieceController>().myX <= 6 && gameObject.GetComponent<PieceController>().myZ >= 2
+            && !ReferenceController.Instance.CheckEmpty(contX, contZ)
+            && ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Knight"
+            && (!ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            || ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite))
+        {
+            ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
+            xPlus1ZReduce2 = true;
+        }
+        else
+        {
+            xPlus1ZReduce2 = false;
+        }
+
+        // X - 1, Z + 2
+        contX = gameObject.GetComponent<PieceController>().myX - 1;
+        contZ = gameObject.GetComponent<PieceController>().myZ + 2;
+
+        if (gameObject.GetComponent<PieceController>().myX >= 1 && gameObject.GetComponent<PieceController>().myZ <= 5
+            && !ReferenceController.Instance.CheckEmpty(contX, contZ)
+            && ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Knight"
+            && (!ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            || ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite))
+        {
+            ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
+            xReduce1ZPlus2 = true;
+        }
+        else
+        {
+            xReduce1ZPlus2 = false;
+        }
+
+        // X - 2, Z + 1
+        contX = gameObject.GetComponent<PieceController>().myX - 2;
+        contZ = gameObject.GetComponent<PieceController>().myZ + 1;
+
+        if (gameObject.GetComponent<PieceController>().myX >= 2 && gameObject.GetComponent<PieceController>().myZ <= 6
+            && !ReferenceController.Instance.CheckEmpty(contX, contZ)
+            && ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Knight"
+            && (!ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            || ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite))
+        {
+            ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
+            xReduce2ZPlus1 = true;
+        }
+        else
+        {
+            xReduce2ZPlus1 = false;
+        }
+
+        // X - 2, Z - 1
+        contX = gameObject.GetComponent<PieceController>().myX - 2;
+        contZ = gameObject.GetComponent<PieceController>().myZ - 1;
+
+        if (gameObject.GetComponent<PieceController>().myX >= 2 && gameObject.GetComponent<PieceController>().myZ >= 1
+            && !ReferenceController.Instance.CheckEmpty(contX, contZ)
+            && ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Knight"
+            && (!ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            || ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite))
+        {
+            ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
+            xReduce2ZReduce1 = true;
+        }
+        else
+        {
+            xReduce2ZReduce1 = false;
+        }
+
+        // X - 1, Z - 2
+        contX = gameObject.GetComponent<PieceController>().myX - 1;
+        contZ = gameObject.GetComponent<PieceController>().myZ - 2;
+
+        if (gameObject.GetComponent<PieceController>().myX >= 1 && gameObject.GetComponent<PieceController>().myZ >= 2
+            && !ReferenceController.Instance.CheckEmpty(contX, contZ)
+            && ReferenceController.Instance.CheckTypePiece(contX, contZ) == "Knight"
+            && (!ReferenceController.Instance.CheckColorPiece(contX, contZ) && GetComponent<PieceController>().isWhite
+            || ReferenceController.Instance.CheckColorPiece(contX, contZ) && !GetComponent<PieceController>().isWhite))
+        {
+            ReferenceController.Instance.PaintKingCheck(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
+            xReduce1ZReduce2 = true;
+        }
+        else
+        {
+            xReduce1ZReduce2 = false;
+        }
+
+        if (!xPlusZPlus && !xReduceZPlus && !xPlusZReduce && !xReduceZReduce
+            && !xPlus && !zPlus && !xReduce && !zReduce
+            && !xPlus1ZPlus2 && !xPlus2ZPlus1 && !xPlus2ZReduce1 && !xPlus1ZReduce2 && !xReduce1ZPlus2 && !xReduce2ZPlus1 && !xReduce2ZReduce1 && !xReduce1ZReduce2)
+
+        {
+            ReferenceController.Instance.ReturnColorKing(gameObject.GetComponent<PieceController>().myX, gameObject.GetComponent<PieceController>().myZ);
+        }
+
+    }
 }
